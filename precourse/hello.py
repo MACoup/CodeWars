b68e25483e0ad0f441967c6e001f3a6f1ae5e307
Mark Coup <MACDaddy@Marks-MacBook-Pro.local>
2016-08-20 15:10:52 -0600

    First ever commit

---
+++ precourse/hello.py
@@ -0,0 +1,17 @@
+import ipdb # Interactive Python debugger
+
+def hello_world():
+    print 'hello, cruel world!'
+
+def add_em_up(a, b, c):
+    return a + b + c
+
+def power_up(b, e):
+    return b ** e # coding=utf-8
+
+if __name__ == '__main__':
+    hello_world()
+    a = 22
+    ipdb.set_trace()
+    b = 33
+    print add_em_up(3, 4, 5)
